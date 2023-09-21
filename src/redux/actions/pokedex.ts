0f785d3d13198a88df0f4132aab117ece138b47f
luisalfredomtz <luisalfredomtz@hotmail.com>
2023-09-21 00:31:18 -0600

    help: remove log

--- src/redux/actions/pokedex.ts
+++ src/redux/actions/pokedex.ts
@@ -12,11 +12,9 @@ export const getPokedex = ({ offset, limit }: { offset: number; limit: number })
       api
         .getPokedex({ offset, limit })
         .then((response: any) => {
-          console.log("🚀 ~ file: pokedex.ts:15 ~ .then ~ response:", response)
           return response;
         })
         .catch((error: any) => {
-          console.log("🚀 ~ file: pokedex.ts:19 ~ getPokedex ~ error:", error)
           return error;
         }),
   };
